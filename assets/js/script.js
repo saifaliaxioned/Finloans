4a227e2849be6570041534878cc80ed31f49af68
saifaliaxioned <saifalis@axioned.com>
2022-10-26 13:10:43 +0530

    accordion function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-/* Author: 
-
-*/
-
-
+// accordion function
+var accordionList = document.querySelectorAll('.accordion-list');
+accordionList.forEach(function (list) {
+  list.addEventListener('click',function () {
+    if (list.classList.contains('active-accordion')) {
+      list.classList.remove('active-accordion');
+    } else {
+      // accordionList.forEach(function (list) {
+      //   if (list.classList.contains('active-list')) {
+      //     list.classList.remove('active-list');
+      //   }
+      // })
+      var activeList = document.querySelector('.active-accordion');
+      if (activeList) {
+        activeList.classList.remove('active-accordion');
+      }
+      list.classList.add('active-accordion');
+    }
+  });
+});
 
 
 
